ea089e89bfc4fa472f404e9f51f40fdf1e55e0f7
1aaronscott <1aaronscott@users.noreply.github.com>
2020-06-17 14:01:12 -0500

    refactor to so load() takes a filename as an arg

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,10 +5,26 @@ import sys
 
 class CPU:
     """Main CPU class."""
-    opcodes = {"HLT": 0b00000001,  # 1
-               "LDI": 0b10000010,  # 130
-               "PRN": 0b01000111,  # 71
-               "MUL": 0b10100010, }  # 162
+    opcodes = {"HLT": 0b00000001,    # 1
+               "LDI": 0b10000010,    # 130
+               "PRN": 0b01000111,    # 71
+               "MUL": 0b10100010,    # 162
+               "PUSH": 0b01000101,   # 69
+               "POP": 0b01000110,    # 70
+               "ADD": 0b10100000,  # 160
+               "SUB": 0b10100001,  # 161
+               "MUL": 0b10100010,  # 162
+               "DIV": 0b10100011,  # 163
+               "MOD": 0b10100100,  # 164
+               "INC": 0b01100101,  # 101
+               "DEC": 0b01100110,  # 102
+               "CMP": 0b10100111,  # 167
+               "AND": 0b10101000,  # 168
+               "NOT": 0b01101001,  # 105
+               "OR ": 0b10101010,  # 170
+               "XOR": 0b10101011,  # 171
+               "SHL": 0b10101100,  # 172
+               "SHR": 0b10101101, }  # 173
 
     def __init__(self):
         """Construct a new CPU."""
@@ -24,14 +40,16 @@ class CPU:
         self.fl = [0]*8  # Flags
         # Program Counter, address of the currently executing instruction
         self.pc = 0
-        self.register = [0]*8  # 8-bit register
+        self.register = [0, 0, 0, 0, 0, 0, 0, 0xf4]  # 8-bit register
         self.branch_table = {self.opcodes["HLT"]: self.hlt,
                              self.opcodes["LDI"]: self.ldi,
                              self.opcodes["PRN"]: self.prn,
-                             self.opcodes["MUL"]: self.mul}
+                             self.opcodes["MUL"]: self.mul, }
+        # self.opcodes["POP"]: self.pop,
+        # self.opcodes["PUSH"]: self.push}
         self.running = True
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
 
         # address = 0
@@ -53,17 +71,17 @@ class CPU:
         #     address += 1
         address = 0
         try:
-            filename = sys.argv[1]
+            #            filename = sys.argv[1]
             with open(filename) as f:
                 for line in f:
                     # split lines into opcodes and other cruft
                     line = line.split("#")[0].strip()
-                    #print(address, line)
+                    # print(address, line)
                     if line == "":  # if no opcode is present
                         continue
                     else:  # add opcode to memory
                         v = int(line, 2)
-                        #print(address, v)
+                        # print(address, v)
                         self.ram[address] = v
                         address += 1
         # handle user not supplying correct data
@@ -88,7 +106,7 @@ class CPU:
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
         # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -138,17 +156,25 @@ class CPU:
     def ldi(self):
         ''' load a value into a register '''
         self.register[self.ram[self.pc+1]] = self.ram[self.pc+2]
+        # print(f'''
+        #       self.pc {self.pc}
+        #       self.ram[self.pc] {self.ram[self.pc]}
+        #       self.register[self.ram[self.pc+1]] {self.register[self.ram[self.pc+1]]}
+        #       self.ram[self.pc] bitshift {(self.ram[self.pc] & 0b11000000 >> 6)+1}''')
+#        self.pc += (self.ram[self.pc] & 0b11000000 >> 6) + 1
         self.pc += 3
 
     def prn(self):
         ''' print value of a register '''
         print(self.register[self.ram[self.pc+1]])
+#        self.pc += (self.ram[self.pc] & 0b11000000 >> 6) + 1
         self.pc += 2
 
     def mul(self):
         ''' multiply to register values '''
         self.register[self.ram[self.pc+1]] = self.register[self.ram[self.pc+2]
                                                            ]*self.register[self.ram[self.pc+1]]
+#        self.pc += (self.ram[self.pc] & 0b11000000 >> 6) + 1
         self.pc += 3
 
     def call_function(self, function):
@@ -156,7 +182,7 @@ class CPU:
         try:
             self.branch_table[function]()
         except KeyError:
-            print(f"This operation ({function}) not yet implemented!")
+            print(f"This operation ({bin(function)}) not yet implemented!")
             exit(1)
         # if self.branch_table[function] is not None:
         #     self.branch_table[function]()
